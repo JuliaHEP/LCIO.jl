e818ffd4e9c5c215303805fb5ec82d8ce7672a4c
Jan Strube <jan.strube@gmail.com>
2016-07-22 19:28:15 -0700

    adding wrapper for CellIDDecoder

--- deps/src/lciowrap/lciowrap.cc
+++ deps/src/lciowrap/lciowrap.cc
@@ -16,14 +16,34 @@
 #include "EVENT/Vertex.h"
 #include "EVENT/LCRelation.h"
 #include "EVENT/LCGenericObject.h"
+#include "UTIL/CellIDDecoder.h"
+#include "UTIL/BitField64.h"
 
 using namespace std;
-
-std::string open() {
-    IOIMPL::LCFactory::getInstance()->createLCReader();
+using namespace cxx_wrap;
+IO::LCReader* createReader() {
+    return IOIMPL::LCFactory::getInstance()->createLCReader();
 }
 
 JULIA_CPP_MODULE_BEGIN(registry)
     cxx_wrap::Module& lciowrap = registry.create_module("lciowrap");
-    lciowrap.method("open", &open);
+    lciowrap.method("createReader", &createReader);
+    
+    lciowrap.add_type<EVENT::SimCalorimeterHit>("SimCalorimeterHit")
+      .method("getEnergy", &EVENT::SimCalorimeterHit::getEnergy);
+    
+    lciowrap.add_type<EVENT::LCCollection>("LCCollection");
+
+    lciowrap.add_type<UTIL::BitField64>("BitField64")
+      .constructor<const string&>();
+    
+    lciowrap.add_type<Parametric<TypeVar<1>>>("CellIDDecoder")
+      .apply<UTIL::CellIDDecoder<EVENT::SimCalorimeterHit>>([](auto wrapped)
+    {
+      // wrapped.constructor<const string&>();
+      typedef typename decltype(wrapped)::type WrappedT;
+      // wrapped.constructor<const EVENT::LCCollection*>();
+      // wrapped.method("find", &WrappedT::operator());
+    });
+
 JULIA_CPP_MODULE_END
