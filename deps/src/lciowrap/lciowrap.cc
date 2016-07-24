0367f648eb59611ddeacd42f1929c1e119dfab13
Jan Strube <jan.strube@gmail.com>
2016-07-23 20:50:55 -0700

    updates to get CxxWrap working

--- deps/src/lciowrap/lciowrap.cc
+++ deps/src/lciowrap/lciowrap.cc
@@ -25,25 +25,79 @@ IO::LCReader* createReader() {
     return IOIMPL::LCFactory::getInstance()->createLCReader();
 }
 
+void deleteReader(IO::LCReader* reader) {
+    delete reader;
+}
+
+// This is just a simple wrapper class around the lccollection pointer
+// This will be constructed together with the type, which can be inferred from
+// the collection name parameter
+template<typename T>
+struct TypedCollection
+{
+    TypedCollection(EVENT::LCCollection* collection) {
+        m_coll = collection;
+    }
+    EVENT::LCCollection* m_coll;
+    inline T* getElementAt(size_t i) {
+        return static_cast<T*>(m_coll->getElementAt(i));
+    }
+    inline size_t getNumberOfElements() {
+        return m_coll->getNumberOfElements();
+    }
+};
+
 JULIA_CPP_MODULE_BEGIN(registry)
     cxx_wrap::Module& lciowrap = registry.create_module("lciowrap");
-    lciowrap.method("createReader", &createReader);
-    
+    lciowrap.method("createLCReader", &createReader);
+    lciowrap.method("deletLCReader", &deleteReader);
+
+    // most of the functionality is forwarded to the TypedCollection
+    lciowrap.add_type<EVENT::LCCollection>("LCCollection")
+        .method("getNumberOfElements", &EVENT::LCCollection::getNumberOfElements)
+        .method("getElementAt", &EVENT::LCCollection::getElementAt)
+        .method("getTypeName", &EVENT::LCCollection::getTypeName);
+
+    lciowrap.add_type<Parametric<TypeVar<1>>>("TypedCollection")
+        .apply<TypedCollection<EVENT::SimCalorimeterHit>
+             , TypedCollection<EVENT::CalorimeterHit>
+             , TypedCollection<EVENT::MCParticle>
+             , TypedCollection<EVENT::ReconstructedParticle>
+             , TypedCollection<EVENT::TrackerHit>
+             , TypedCollection<EVENT::SimTrackerHit>
+             , TypedCollection<EVENT::LCRelation>
+             , TypedCollection<EVENT::LCGenericObject>
+             , TypedCollection<EVENT::Track>
+             , TypedCollection<EVENT::Vertex>>([](auto wrapped)
+        {
+        typedef typename decltype(wrapped)::type WrappedT;
+        wrapped.template constructor<EVENT::LCCollection*>();
+        wrapped.method("getElementAt", &WrappedT::getElementAt);
+        wrapped.method("getNumberOfElements", &WrappedT::getNumberOfElements);
+    });
+    lciowrap.add_type<EVENT::LCEvent>("LCEvent")
+        .method("getCollection", &EVENT::LCEvent::getCollection)
+        .method("getCollectionNames", &EVENT::LCEvent::getCollectionNames);
+
+    lciowrap.add_type<IO::LCReader>("LCReader");
+        // .method("open", &IO::LCReader::open)
+        // .method("readNextEvent", &IO::LCReader::readNextEvent)
+        // .method("close", &IO::LCReader::close);
+
     lciowrap.add_type<EVENT::SimCalorimeterHit>("SimCalorimeterHit")
-      .method("getEnergy", &EVENT::SimCalorimeterHit::getEnergy);
-    
-    lciowrap.add_type<EVENT::LCCollection>("LCCollection");
+        .method("getEnergy", &EVENT::SimCalorimeterHit::getEnergy);
 
     lciowrap.add_type<UTIL::BitField64>("BitField64")
-      .constructor<const string&>();
-    
+        .constructor<const string&>()
+        .method("getValue", &UTIL::BitField64::getValue);
+
     lciowrap.add_type<Parametric<TypeVar<1>>>("CellIDDecoder")
-      .apply<UTIL::CellIDDecoder<EVENT::SimCalorimeterHit>>([](auto wrapped)
+        .apply<UTIL::CellIDDecoder<EVENT::SimCalorimeterHit>>([](auto wrapped)
     {
-      // wrapped.constructor<const string&>();
-      typedef typename decltype(wrapped)::type WrappedT;
-      // wrapped.constructor<const EVENT::LCCollection*>();
-      // wrapped.method("find", &WrappedT::operator());
+        typedef typename decltype(wrapped)::type WrappedT;
+        wrapped.template constructor<const string&>();
+        wrapped.template constructor<const EVENT::LCCollection*>();
+        wrapped.method("get", &WrappedT::operator());
     });
 
 JULIA_CPP_MODULE_END
--- src/LCIO.jl
+++ src/LCIO.jl
@@ -1,6 +1,157 @@
 module LCIO
 using CxxWrap
+import Base: getindex, start, done, next, length, +
 
 wrap_module("liblciowrap")
+
+function __init__()
+    global reader = createLCReader()
+    atexit() do
+        # CxxWrap'ed objects automatically call delete in the finalizer
+        #deletLCReader(reader)
+    end
+end
+
+export Vec, getCollection, getCollectionNames, getCollectionTypeName, getP4, getPosition
+
+immutable Vec
+	x::Cdouble
+	y::Cdouble
+	z::Cdouble
+	t::Cdouble
+end
++(a::Vec, b::Vec) = Vec(a.x+b.x, a.y+b.y, a.z+b.z, a.t+b.t)
+
+immutable ThreeVec
+	x::Cdouble
+	y::Cdouble
+	z::Cdouble
+end
++(a::ThreeVec, b::ThreeVec) = ThreeVec(a.x+b.x, a.y+b.y, a.z+b.z)
+
+immutable CalHit
+	x::Cfloat
+	y::Cfloat
+	z::Cfloat
+	E::Cfloat
+end
+
+# typealias VectorStringP cxxt"const std::vector<std::string>*"
+#
+# start(it::VectorStringP) = 0
+# next(it::VectorStringP,i) = (it[i], i+1)
+# done(it::VectorStringP,i) = i >= length(it)
+# getindex(it::VectorStringP,i) = String( icxx"$(it)->at($i).c_str();" )
+# length(it::VectorStringP) = icxx"$(it)->size();"
+
+# The iterators use the events as state
+# the lcio reader returns NULL at the end of the file
+# Because the LCIO getNext function could re-use the memory for the next event,
+# the next function should not hold the current and next event at the same time.
+# Returning current and reading nextEvent as the new state causes memory corruption
+
+type EventIterator
+	current::LCEvent
+end
+start(it::EventIterator) = C_NULL
+next(it::EventIterator, state) = (it.current, C_NULL)
+function done(it::EventIterator, state)
+	it.current = readNextEvent(reader)
+	isdone = it.current == C_NULL
+	if isdone
+		close(reader)
+	end
+	isdone
+end
+
+# open file with reader, returns iterator
+function open(fn::AbstractString)
+	open(reader, fn)
+	# returns an iterator, initialized with a nullptr
+	# the iterator knows about the global reader object
+	return EventIterator( nothing )
+end
+
+# We would like to have a typed collection, but what getCollection returns is unfortunately untyped
+# The type is established by reading its name from the collection and mapping it in the LCIOTypemap
+immutable LCCollection{T}
+	coll::LCCollection
+end
+
+# typealias SimCalorimeterHit cxxt"EVENT::SimCalorimeterHit*"
+# typealias TrackerHit cxxt"EVENT::TrackerHit*"
+# typealias SimTrackerHit cxxt"EVENT::SimTrackerHit*"
+# typealias MCParticle cxxt"EVENT::MCParticle*"
+# typealias Track cxxt"EVENT::Track*"
+# typealias LCGenericObject cxxt"EVENT::LCGenericObject*"
+#
+
+# map from names stored in collection to actual types
+LCIOTypemap = Dict(
+	"SimCalorimeterHit" => SimCalorimeterHit,
+	"TrackerHit" => TrackerHit,
+	"SimTrackerHit" => SimTrackerHit,
+	"MCParticle" => MCParticle,
+	"Track" => Track,
+	"LCGenericObject" => LCGenericObject,
+)
+
+start(it::LCCollection) = 0
+done(it::LCCollection, i) = i >= length(it)
+next{T}(it::LCCollection{T}, i) = getElementAt{T}(it.coll, i), i+1
+length(it::LCCollection) = getNumberOfElements(it.coll)
+
+function getCollection(event, collectionName)
+	collection = getCollection(event, collectionName)
+	collectionType = getTypeName(collection)
+	return TypedCollection{LCIOTypemap[String(collectionType)]}(collection)
+end
+
+# getEnergy(particle) = icxx"$(particle)->getEnergy();"
+# function getMomentum(particle)
+# 	p3 = icxx"$(particle)->getMomentum();"
+# 	ThreeVec(unsafe_load(p3, 1), unsafe_load(p3, 2), unsafe_load(p3, 3))
+# end
+# function getPosition(hit)
+# 	pos = icxx"$(hit)->getPosition();"
+# 	ThreeVec(unsafe_load(pos, 1), unsafe_load(pos, 2), unsafe_load(pos, 3))
+# end
+#
+# function getP4(particle)
+# 	p3 = icxx"$(particle)->getMomentum();"
+# 	e = icxx"$(particle)->getEnergy();"
+# 	Vec(unsafe_load(p3, 1), unsafe_load(p3, 2), unsafe_load(p3, 3), e)
+# end
+
+
+# include("MCParticle.jl")
+# include("CaloHit.jl")
+
+# getRelationFrom(mcp::Ptr{Void}) = ccall((:lcrelgetfrom, libLCIO), Ptr{Void}, (Ptr{Void}, ), mcp)
+# getRelationTo(mcp::Ptr{Void}) = ccall((:lcrelgetto, libLCIO), Ptr{Void}, (Ptr{Void}, ), mcp)
+
+
+# getPositionVec(hit::Ptr{Void}) = ccall((:lcsthgetposition, libLCIO), ThreeVec, (Ptr{Void}, ), hit)
+#
+# function getPosition(hit::Ptr{Void})
+# 	pos = ccall((:lcsthgetposition, libLCIO), Ptr{Cdouble}, (Ptr{Void},), hit)
+# 	return [unsafe_load(pos, 1) unsafe_load(pos, 2) unsafe_load(pos, 3)]
+# end
+#
+#
+# function getParticleHits(rp::Ptr{Void})
+# 	hitList = CalHit[]
+# 	nClusters = Ref{Csize_t}(0)
+# 	cList = ccall((:lcrcpgetclusters, libLCIO), Ptr{Ptr{Void}}, (Ptr{Void}, Ref{Csize_t}), rp, nClusters)
+# 	for i in 1:nClusters[]
+# 		nHits = Ref{Csize_t}(0)
+# 		hitCollection = ccall((:lcclugetcalorimeterhits, libLCIO), Ptr{Ptr{Void}}, (Ptr{Void}, Ref{Csize_t}), unsafe_load(cList, i), nHits)
+# 		for j in 1:nHits[]
+# 			push!(hitList, getCaloHit(unsafe_load(hitCollection, j)))
+# 		end
+# 	end
+# 	return hitList
+# end
+
 
 end
