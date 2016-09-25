116cc1aef9c0d1b36419118438170749d2e7ff2b
Jan Strube <jan.strube@gmail.com>
2016-09-24 23:38:37 -0700

    updating build to use github tarball instead of freehep svn

--- deps/build.jl
+++ deps/build.jl
@@ -9,11 +9,11 @@ cxx_wrap_dir = Pkg.dir("CxxWrap","deps","usr","lib","cmake")
 
 liblciowrap = library_dependency("liblciowrap")
 prefix=joinpath(BinDeps.depsdir(liblciowrap),"usr")
-lcioversion = "v02-07-02"
+lcioversion = "02-07-02"
 # if the LCIO environment has been sourced, use it. Otherwise, download from svn
 lciowrap_srcdir = joinpath(BinDeps.depsdir(liblciowrap), "src", "lciowrap")
 lciowrap_builddir = joinpath(BinDeps.depsdir(liblciowrap),"builds","lciowrap")
-lcio_srcdir = haskey(ENV, "LCIO") ? ENV["LCIO"] : joinpath(lciowrap_builddir, lcioversion)
+lcio_srcdir = haskey(ENV, "LCIO") ? ENV["LCIO"] : joinpath(lciowrap_builddir, "LCIO-$(lcioversion)")
 lib_prefix = @static is_windows() ? "" : "lib"
 lib_suffix = @static is_windows() ? "dll" : (@static is_apple() ? "dylib" : "so")
 
@@ -24,7 +24,8 @@ genopt = "Unix Makefiles"
 provides(BuildProcess,
   (@build_steps begin
     FileRule(joinpath(lcio_srcdir, "LCIOConfig.cmake"), @build_steps begin
-        `svn co svn://svn.freehep.org/lcio/tags/"$lcioversion" "$lcio_srcdir"`
+        FileDownloader("https://github.com/iLCSoft/LCIO/archive/v$(lcioversion).tar.gz", joinpath(lciowrap_builddir,"$(lcioversion).tar.gz"))
+        FileUnpacker(joinpath(lciowrap_builddir,"$(lcioversion).tar.gz"), lciowrap_builddir, lcio_srcdir)
         CreateDirectory(joinpath(lcio_srcdir, "build"))
         @build_steps begin
             ChangeDirectory(joinpath(lcio_srcdir, "build"))
@@ -33,7 +34,6 @@ provides(BuildProcess,
             `make install`
         end
     end)
-    CreateDirectory(lciowrap_builddir)
     @build_steps begin
       ChangeDirectory(lciowrap_builddir)
       FileRule(joinpath(prefix, "lib$libdir_opt", "$(lib_prefix)lciowrap.$lib_suffix"), @build_steps begin
