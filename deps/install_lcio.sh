c0601b85d37dcb708077c97b16da9df3ed47e4c6
Jan Strube <jan.strube@gmail.com>
2016-09-26 18:52:08 -0700

    travis fix

--- deps/install_lcio.sh
+++ deps/install_lcio.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 curl -OL https://github.com/iLCSoft/LCIO/archive/v02-07-02.tar.gz
 tar xzf v02-07-02.tar.gz
-mv LCIO-02-07-02 ${HOME}/lcio_bindir
-cd ${HOME}/lcio_bindir
+mv LCIO-02-07-02 ${HOME}/lcio_bindir/lcio
+cd ${HOME}/lcio_bindir/lcio
 mkdir build
 cd build
 cmake ..
 make
 make install
-cd ${HOME}
