bc74ba4e44c452b04bf2e9d464c7701f7316e2e0
Jan Strube <jan.strube@gmail.com>
2016-09-26 17:18:46 -0700

    More updates to the build setup to separate the lcio libs from the LCIO.jl sources for the sake of caching

---
+++ deps/install_lcio.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+wget https://github.com/iLCSoft/LCIO/archive/v02-07-02.tar.gz
+tar xzf v02-07-02.tar.gz
+mv LCIO-02-07-02 ${HOME}/lcio_bindir
+cd ${HOME}/lcio_bindir
+mkdir build
+cd build
+cmake ..
+make
+make install
+cd ${HOME}
