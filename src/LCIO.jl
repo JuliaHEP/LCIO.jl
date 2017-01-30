933eaa348e40a4282998e686c96a19651788fe2d
Jan Strube <jan.strube@gmail.com>
2017-01-30 14:28:36 -0800

    Update LCIO.jl

--- src/LCIO.jl
+++ src/LCIO.jl
@@ -44,7 +44,7 @@ getindex(it::StdVecs, i) = at(it, i-1)
 
 start(it::LCReader) = getNumberOfEvents(it)
 next(it::LCReader, state) = readNextEvent(it), state-1
-done(it::LCReader, state) = state <= 1
+done(it::LCReader, state) = state < 1
 length(it::LCReader) = getNumberOfEvents(it)
 
 function iterate(f::Function, fn::AbstractString)
