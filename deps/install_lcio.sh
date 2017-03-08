add699785a261b979978c7d12861a021de24cbcd
Jan Strube <jan.strube@gmail.com>
2017-03-07 20:49:39 -0800

    Update to LCIO v020704 (#21)
    * Update to LCIO v020704
    
    * Update build.jl

--- deps/install_lcio.sh
+++ deps/install_lcio.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-curl -OL https://github.com/iLCSoft/LCIO/archive/v02-07-02.tar.gz
-tar xzf v02-07-02.tar.gz
-mv LCIO-02-07-02 ${HOME}/lcio_bindir/lcio
+LCIOVER="02-07-04"
+curl -OL https://github.com/iLCSoft/LCIO/archive/v${LCIOVER}.tar.gz
+tar xzf v${LCIOVER}.tar.gz
+mv LCIO-${LCIOVER} ${HOME}/lcio_bindir/lcio
 cd ${HOME}/lcio_bindir/lcio
 mkdir build
 cd build
