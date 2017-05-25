f3eeb0f1e1cd273d9261ffc943f43dfed2aaaa11
Jan Strube <jan.strube@gmail.com>
2017-05-24 18:06:05 -0700

    Fixing a typo that didn't show up on Mac OS X and fixing the RPATH for the Linux build

--- deps/build.jl
+++ deps/build.jl
@@ -43,7 +43,7 @@ provides(BuildProcess,
     @build_steps begin
       ChangeDirectory(lciowrap_builddir)
       FileRule(joinpath(prefix, "lib$libdir_opt", "$(lib_prefix)lciowrap.$lib_suffix"), @build_steps begin
-      	`cmake -G "$genopt" -DCMAKE_INSTALL_PREFIX="$prefix" -DCMAKE_BUILD_TYPE="Release" -DJLCxx_DIR="$jlcxx_dir" -DLIBDIR_SUFFIX="$libdir_opt" -DLCIO_INSTALLDIR="$lcio_srcdir"  $lciowrap_srcdir`
+      	`cmake -G "$genopt" -DCMAKE_INSTALL_PREFIX="$prefix" -DCMAKE_BUILD_TYPE="Release" -DJlCxx_DIR="$jlcxx_dir" -DLIBDIR_SUFFIX="$libdir_opt" -DLCIO_INSTALLDIR="$lcio_srcdir"  $lciowrap_srcdir`
         `make`
         `make install`
       end)
