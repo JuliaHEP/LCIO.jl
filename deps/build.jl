c14d85777183020f55b4a72369176295c1a5bd4e
Jan Strube <jan.strube@gmail.com>
2017-05-27 10:13:34 -0700

    Going back to using cache on travis

--- deps/build.jl
+++ deps/build.jl
@@ -3,34 +3,33 @@ using BinDeps
 
 lcioversion = "02-08"
 
-libdir_opt = ""
 @BinDeps.setup
 
 jlcxx_dir = Pkg.dir("CxxWrap","deps","usr","share","cmake","JlCxx")
 
-liblcio = library_dependency("liblcio")
 liblciowrap = library_dependency("liblciowrap")
-prefix=joinpath(BinDeps.depsdir(liblciowrap),"usr")
-# if the LCIO environment has been sourced, use it. Otherwise, download from svn
-lciowrap_builddir = joinpath(BinDeps.depsdir(liblciowrap),"builds","lciowrap")
-lciowrap_srcdir = joinpath(BinDeps.depsdir(liblciowrap), "src", "lciowrap")
-# use a generic, unversioned dir name to simplify installation and keep travis installation
-# reasonably symmetric with this version
-lcio_srcdir = joinpath(BinDeps.depsdir(liblcio), "src", "LCIO-$(lcioversion)")
-lcio_destdir = prefix
+prefix = BinDeps.depsdir(liblciowrap)
+downloads = joinpath(prefix, "downloads")
+lciowrap_builddir = joinpath(prefix, "builds", "lciowrap")
+lciowrap_srcdir = joinpath(prefix, "src", "lciowrap")
+lciowrap_destdir = joinpath(prefix, "usr")
+lcio_srcdir = joinpath(prefix, "src", "LCIO-$(lcioversion)")
+# if the LCIO environment has been sourced, use it. Otherwise, download the source and build
+lcio_destdir = haskey(ENV, "LCIO") ? ENV["LCIO"] : joinpath(prefix, "usr")
+
+libdir_opt = ""
 lib_prefix = @static is_windows() ? "" : "lib"
 lib_suffix = @static is_windows() ? "dll" : (@static is_apple() ? "dylib" : "so")
 
-# LCIOConfig.cmake needs to be available. If not there, check out the LCIO source
-# unfortunately, this only works on POSIX at the moment
 lcio_library = joinpath(lcio_destdir, "lib", "$(lib_prefix)lcio.$(lib_suffix)")
+lciowrap_library = joinpath(prefix, "usr", "lib$libdir_opt", "$(lib_prefix)lciowrap.$(lib_suffix)")
+
 genopt = "Unix Makefiles"
 
 # remove the previous libraries -- always build at least the library
-isfile(joinpath(prefix, "lib$libdir_opt", "$(lib_prefix)lciowrap.$(lib_suffix)")) && rm(joinpath(prefix, "lib$(libdir_opt)", "$(lib_prefix)lciowrap.$(lib_suffix)"))
+isfile(lciowrap_library) && rm(lciowrap_library)
 isfile(joinpath(lciowrap_builddir, "lib$libdir_opt", "$(lib_prefix)lciowrap.$(lib_suffix)")) && rm(joinpath(lciowrap_builddir, "lib$(libdir_opt)", "$(lib_prefix)lciowrap.$(lib_suffix)"))
 
-provides(Sources, URI("https://github.com/iLCSoft/LCIO/archive/v$(lcioversion).tar.gz"), liblcio, unpacked_dir="LCIO-$(lcioversion)")
 provides(BuildProcess,
   (@build_steps begin
 		# we're looking for the cmake file, even though the dependencies are the libs
@@ -38,7 +37,10 @@ provides(BuildProcess,
         CreateDirectory(lciowrap_builddir)
         @build_steps begin
             ChangeDirectory(lciowrap_builddir)
-						GetSources(liblcio)
+            FileDownloader("https://github.com/iLCSoft/LCIO/archive/v$(lcioversion).tar.gz", joinpath(downloads, "v$(lcioversion).tar.gz"))
+						CreateDirectory(joinpath(prefix, "src"))
+            FileUnpacker(joinpath(downloads, "v$(lcioversion).tar.gz"), joinpath(prefix, "src"), "LCIO-$(lcioversion)")
+						# GetSources(liblcio)
 						CreateDirectory(joinpath(lcio_srcdir, "build"))
 						@build_steps begin
 							ChangeDirectory(joinpath(lcio_srcdir, "build"))
@@ -48,16 +50,11 @@ provides(BuildProcess,
 						end
         end
     end)
-	end), liblcio,
-	)
-
-provides(BuildProcess,
-  (@build_steps begin
     CreateDirectory(lciowrap_builddir)
     @build_steps begin
       ChangeDirectory(lciowrap_builddir)
-      FileRule(joinpath(prefix, "lib$libdir_opt", "$(lib_prefix)lciowrap.$lib_suffix"), @build_steps begin
-      	`cmake -G "$genopt" -DCMAKE_INSTALL_PREFIX="$prefix" -DCMAKE_BUILD_TYPE="Release" -DJlCxx_DIR="$jlcxx_dir" -DLIBDIR_SUFFIX="$libdir_opt" -DLCIO_DIR="$lcio_srcdir"  $lciowrap_srcdir`
+      FileRule(lciowrap_library, @build_steps begin
+      	`cmake -G "$(genopt)" -DCMAKE_INSTALL_PREFIX="$(lciowrap_destdir)" -DCMAKE_BUILD_TYPE="Release" -DJlCxx_DIR="$(jlcxx_dir)" -DLCIO_DIR="$(lcio_destdir)"  $(lciowrap_srcdir)`
         `make`
         `make install`
       end)
